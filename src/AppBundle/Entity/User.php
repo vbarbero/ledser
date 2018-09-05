89a4d21268299499dff6b11c0606025e65dd3f67
victor <victor@parclick.com>
2018-09-05 22:01:24 +0200

    primer

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -31,9 +31,50 @@ class User extends BaseUser
      */
     protected $proposal;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $name;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
     }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return Contact
+     */
+    public function getProposal(): Contact
+    {
+        return $this->proposal;
+    }
+
+    /**
+     * @param Contact $proposal
+     */
+    public function setProposal(Contact $proposal)
+    {
+        $this->proposal = $proposal;
+    }
+
+
 }
