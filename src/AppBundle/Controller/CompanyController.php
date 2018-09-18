1347419352d1d8d112374e7f9c753e8593459e1b
victor <victor@parclick.com>
2018-09-19 01:03:23 +0200

    asdf

--- src/AppBundle/Controller/CompanyController.php
+++ src/AppBundle/Controller/CompanyController.php
@@ -34,10 +34,12 @@ class CompanyController extends Controller
         $form = $this->createForm(CompanyFilterType::class, $companyFilterModel);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            echo 1;
             $companyFilterModel = $form->getData();
             $companies = $this->getDoctrine()->getManager()->getRepository(Company::class)->getCompanies($companyFilterModel->getType());
         } else
         {
+            echo 2;
             $companies = $this->getDoctrine()->getManager()->getRepository(Company::class)->getCompanies();
         }
         return $this->render('AppBundle:Company:list.html.twig', ['companies' => $companies, 'form' => $form->createView()]);
