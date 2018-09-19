6d5c2cf695d4e0079e902793b1f0c8e82727dc4d
victor <victor@parclick.com>
2018-09-19 02:52:58 +0200

    asdf

--- src/AppBundle/Model/ProposalModel.php
+++ src/AppBundle/Model/ProposalModel.php
@@ -11,7 +11,30 @@ namespace AppBundle\Model;
 
 class ProposalModel
 {
+    const PROPONENT = 1;
+    const FREED = 2;
+    const CLIENT = 3;
 
+
+    public static function getTypeAsChoice()
+    {
+        return
+            [
+                'PROPONENT' => self::PROPONENT,
+                'FREED' => self::FREED,
+                'CLIENT' => self::CLIENT
+            ];
+    }
+
+    public static function getType()
+    {
+        return
+            [
+                self::PROPONENT => 'PROPONENT',
+                self::FREED => 'FREED',
+                self::CLIENT => 'CLIENT'
+            ];
+    }
 
     public static function getOperationalTypeAsChoice()
     {
