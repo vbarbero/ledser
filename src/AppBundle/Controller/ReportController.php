03b73298f1567f002bf6354eb42753de38ab2b78
victor <victor@parclick.com>
2018-10-22 20:49:52 +0200

    bugfixing

--- src/AppBundle/Controller/ReportController.php
+++ src/AppBundle/Controller/ReportController.php
@@ -100,17 +100,17 @@ class ReportController extends Controller
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $calendarModel = $form->getData();
-            dump($calendarModel->getClientType());
-            dump($request->request->get('clientType'));
-            dump($request->request->all());
+//            dump($calendarModel->getClientType());
+//            dump($request->request->get('clientType'));
+//            dump($request->request->all());
             //$calendarModel->setClientType($request->request->get('clientType'));
         } else
         {
             dump($form->getErrors(true));
         }
         $reports = $calculators = $clients = [];
-        $calendarModel->setClientType(1);
-        dump($calendarModel->getClientType());
+//        $calendarModel->setClientType(1);
+//        dump($calendarModel->getClientType());
         if($calendarModel->getClientType() === 1 || is_null($calendarModel->getClientType()))
         {
             $reports = $this->getDoctrine()->getRepository(Report::class)->getReportToCalendar($calendarModel->getUser(), $thisMonth, $nextMonth);
