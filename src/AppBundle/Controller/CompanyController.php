d80a8f3348f68b4eb80f6806f1b0755b8d5b4a14
victor <victor@parclick.com>
2018-11-05 00:03:20 +0100

    risas

--- src/AppBundle/Controller/CompanyController.php
+++ src/AppBundle/Controller/CompanyController.php
@@ -88,6 +88,7 @@ class CompanyController extends Controller
             $proposalGroup[$calculator->getProposal()->getCompany()->getId()]['tae'] += $calculator->getCosteFinanciero()->getTae();
             $proposalGroup[$calculator->getProposal()->getCompany()->getId()]['coste'] += $calculator->getCosteFinanciero()->getCoste();
             $proposalGroup[$calculator->getProposal()->getCompany()->getId()]['taeLedser'] += $calculator->getCosteFinancieroLedser()->getTae();
+            $proposalGroup[$calculator->getProposal()->getCompany()->getId()]['costeLedser'] += $calculator->getCosteFinancieroLedser()->getCoste();
             $proposalGroup[$calculator->getProposal()->getCompany()->getId()]['total'] += $calculator->getCosteTotal()->getTotal();
             uksort($proposalGroup, function ($ak, $bk) use ($proposalGroup) {
                 $a = $proposalGroup[$ak];
