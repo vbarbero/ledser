1676a83e70ac4bdb287405e06963bf26cde5168d
vb <v.barbero.c@gmail.com>
2019-02-07 19:45:22 +0100

    ds

--- src/AppBundle/Form/Type/CalculatorType.php
+++ src/AppBundle/Form/Type/CalculatorType.php
@@ -62,7 +62,7 @@ class CalculatorType extends AbstractType
         $builder->add('drawee', EntityType::class, [
             'class' => Company::class,
             'choice_label' => 'name',
-            'required' => true,
+            'required' => false,
             'query_builder' => function( CompanyRepository $er ) {
                 return $er->getDrawee();
             }]);
