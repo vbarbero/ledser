42e4336ba680b34328e9fc3f140b345c5bbdfdd0
victor barbero <victormanuel.barberocosgaya@altran.com>
2019-03-03 20:08:43 +0100

    dfd

--- src/AppBundle/Entity/Proposal.php
+++ src/AppBundle/Entity/Proposal.php
@@ -20,7 +20,7 @@ class Proposal extends BaseEntity
 
     /**
      * @var Agent
-     *
+     * @ORM\Column(nullable=true)
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Agent", inversedBy="proposal")
      */
     private $agent;
