ad47561dbe8952028fc9ff9304b121bb6075397f
victor barbero <victormanuel.barberocosgaya@altran.com>
2019-04-11 00:54:48 +0200

    d

--- src/AppBundle/Controller/ProposalController.php
+++ src/AppBundle/Controller/ProposalController.php
@@ -258,7 +258,7 @@ class ProposalController extends Controller
                 $costeTotal = new Cost();
                 $costeTotal->setCoste($costeFinancieroLedser->getCoste() + $calculadora->getHonorarios() + ($remesa->getOmf() + $remesa->getMensajeria() + $remesa->getBurofax() + $remesa->getGastos()) / 4);
                 $costeTotal->setTae((36000 * $costeTotal->getCoste()) / ($remesaCalculadora->getDias() * $remesaCalculadora->getNominal()));
-                $costeTotal->setTotal($costeTotal->getCoste() * 100/ $costeTotal->getNominal());
+                $costeTotal->setTotal($costeTotal->getCoste() * 100/ $remesaCalculadora->getNominal());
                 $costeTotal->setMensual($costeTotal->getTae()/ 12);
                 $costeTotal->setRetencion(0);
                 $costeTotal->setNominal($remesaCalculadora->getNominal());
