69770f414c976da1699b5442748170ea056180e8
victor barbero <victormanuel.barberocosgaya@altran.com>
2019-04-11 00:59:46 +0200

    d

--- src/AppBundle/Controller/ProposalController.php
+++ src/AppBundle/Controller/ProposalController.php
@@ -231,6 +231,8 @@ class ProposalController extends Controller
                 $calculadora->setMensajeria($remesa->getMensajeria());
                 $calculadora->setTimbres($remesaCalculadora->getTimbres());
                 $calculadora->setNominal($remesaCalculadora->getNominal());
+                $calculadora->setNotice($remesa->isNotice());
+                $calculadora->setNoticeMessage($remesa->getNoticeMessage());
 
                 $costeFinanciero = new Cost();
                 $costeFinanciero->setTae($remesaCalculadora->getTae());
