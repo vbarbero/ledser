295b1ac22453452fb9ef2fef4050853f8753ac8a
victor barbero <victormanuel.barberocosgaya@altran.com>
2019-04-24 00:47:10 +0200

    d

--- src/AppBundle/Controller/CompanyController.php
+++ src/AppBundle/Controller/CompanyController.php
@@ -334,7 +334,7 @@ die;*/
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $company = $form->getData();
-            $company->setLatitude($_POST['company']['latitude']);die;
+            $company->setLatitude($_POST['company']['latitude']);
             $em = $this->getDoctrine()->getManager();
             $em->persist($company);
             $em->flush();
