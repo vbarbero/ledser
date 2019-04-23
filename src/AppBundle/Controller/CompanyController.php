4872ffc875432d0a6142fafb79720c613a884551
victor barbero <victormanuel.barberocosgaya@altran.com>
2019-04-24 01:16:24 +0200

    d

--- src/AppBundle/Controller/CompanyController.php
+++ src/AppBundle/Controller/CompanyController.php
@@ -420,7 +420,7 @@ die;*/
             $em = $this->getDoctrine()->getManager();
             $em->persist($contact);
             $em->flush();
-            return $this->redirect("show-company/" . $contact->getCompany()->getId());
+            return $this->redirect($this->generateUrl("show_company", ['company' => $company->getId()]));
         }
         return $this->render('AppBundle:Company:createContact.html.twig', ['form' => $form->createView()]);
     }
